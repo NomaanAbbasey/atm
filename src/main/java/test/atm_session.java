550912eede16ad09e1f16f15e59f41cce49b1dfa
Nomaan <NomaanTheSupreme@bitbucket.org>
2024-10-23 21:22:08 -0400

    added atm session testing class

---
+++ src/main/java/test/atm_session.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class atm_session {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
