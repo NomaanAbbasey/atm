8e8b42eb616f488c819718e26317d5bf4f0db796
Nomaan <NomaanTheSupreme@bitbucket.org>
2024-10-24 21:54:29 -0400

    5 tests for pin completed

--- src/main/java/test/atm_session.java
+++ src/main/java/test/atm_session.java
@@ -1,14 +1,45 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+import atm.ATM;
+import atm.Session;
+import banking.Money;
+import banking.exceptions.*;
 
 class atm_session {
-
+	
+	ATM atm_test = new ATM(0, "London", "CIBC", null);
+	Money test_money = new Money(100);
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_1() {
+		Session test_session = new Session(atm_test, 4567, "12345", 3, 0, 1, test_money);
+		assertDoesNotThrow(() -> test_session.performSession());
+	}
+	
+	@Test
+	void test_2() {
+		Session test_session = new Session(atm_test, 4567, "1234", 3, 0, 1, test_money);
+		Exception exception = assertThrows(InvalidPINException.class, () -> {
+			test_session.performSession();  
+        });
+	}
+	
+	@Test
+	void test_3() {
+		Session test_session = new Session(atm_test, 4567, "123456", 3, 0, 1, test_money);
+		Exception exception = assertThrows(InvalidPINException.class, () -> {
+			test_session.performSession();  
+        });
+	}
+	
+	@Test
+	void test_4() {
+		Session test_session = new Session(atm_test, 4567, "", 3, 0, 1, test_money);
+		Exception exception = assertThrows(InvalidPINException.class, () -> {
+			test_session.performSession();  
+        });
 	}
 
 }
